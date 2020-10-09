7164838162e2090ef45932b744de88bdc5bae0a9
AnthonyQR <55644542+AnthonyQR@users.noreply.github.com>
2020-10-08 21:29:04 -0700

    Finished checkboxes in registration page. Added password boxes, register button, and text to lead to sign in (not actually implemented). Also changed alignment. Sizes are probably not mobile friendly.

--- src/App.js
+++ src/App.js
@@ -11,10 +11,69 @@ function App() {
                 <div class="formbackground">
                       <h1 class="appheader">Musician Registration</h1>
                       <form>
-                          <p></p>
-                          <label>First Name: </label>
-                          <input></input>
+                          <div class="boxesdiv">
+                              <label class="labeltext">First Name: </label>
+                              <input placeholder="First Name" class="inputboxes"></input>
+
+                              <label class="labeltext">Last Name: </label>
+                              <input placeholder="Last Name" class="inputboxes"></input>
+                              <br />
+
+                              <label class="labeltext">Username: </label>
+                              <input placeholder="Username" class="inputboxes"></input>
+                              
+                              <label class="labeltext">Email: </label>
+                              <input placeholder="Email" class="inputboxes"></input>
+                              <br />
+
+                              <label class="labeltext">Number: </label>
+                              <input placeholder="Number" class="inputboxes"></input>
+                              
+                              <label class="labeltext">Address: </label>
+                              <input placeholder="Address" class="inputboxes"></input>
+                              <br />
+
+                              <label class="labeltext">City: </label>
+                              <input placeholder="City" class="inputboxes"></input>
+                              
+                              <label class="labeltext">Province: </label>
+                              <input placeholder="Province" class="inputboxes"></input>
+                              <br />
+                              <br />
+                          </div>
+
+                          <h2 class="checkboxheader">Modes of Transportation</h2>
                           <br />
+
+                          <div class="checkboxdiv">
+                              <input type="checkbox" class="inputcheckbox"></input>
+                              <label class="labelcheckbox">Public Transit</label>
+
+                              <input type="checkbox" class="inputcheckbox"></input>
+                              <label class="labelcheckbox">Car</label>
+
+                              <input type="checkbox" class="inputcheckbox"></input>
+                              <label class="labelcheckbox">Walk</label>
+
+                              <input type="checkbox" class="inputcheckbox"></input>
+                              <label class="labelcheckbox">Other</label>
+                          </div>
+                          <br/>
+                          <br />
+
+                          <div class="passwordboxesdiv">
+                              <label class="passwordlabel">Password: </label>
+                              <input placeholder="Password" class="passwordboxes"></input>
+                              
+                              <label class="passwordlabel">Confirm Password: </label>
+                              <input placeholder="Confirm Password" class="passwordboxes"></input>
+                              <br />
+                          </div>
+
+                          <button class="registerbutton">Register</button>
+                          <br/>
+                          <br/>
+                          <p class="signin">Already have an account? Sign in!</p>
                       </form>
                 </div>
         </div>
