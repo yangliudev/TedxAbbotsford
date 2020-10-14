68ddd62862187392036845005174d06dd2052528
karanharika <55457934+karanharika@users.noreply.github.com>
2020-10-13 20:24:35 -0700

    All set for fun!!
    Order process going on..
    Taking in billing data

--- src/App.js
+++ src/App.js
@@ -1,19 +1,75 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import Navbar from 'react-bootstrap/Navbar';
-// import Button from '@material-ui/core/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+// import { Form } from "react-bootstrap";
+import Container from "@material-ui/core/Container";
+import TextField from '@material-ui/core/TextField';
+import { ThemeProvider, makeStyles, createMuiTheme,} from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
 
 
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: '25ch',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: grey,
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    
-    <div className="App">
+
+    <div>
       <Navbar/>
-      
-  </div>
+      < br />
+    <Container maxWidth="sm" className="Container-one rounded" >
+
+    {/* <form className={classes.root} noValidate autoComplete="off">
+      <label>Enter Name of of Beneficiary:</label> <br />
+      <TextField  InputLabelProps={{style: { color: '#fff' },}} id="outlined-basic" label="First Name" variant="outlined"/>
+      <TextField  InputLabelProps={{style: { color: '#fff' },}} id="outlined-basic" label="Last Name" variant="outlined" />
+    </form> */}
+
+        <form className={classes.root} noValidate>
+        <label>Enter Name of of Beneficiary:</label>
+          <ThemeProvider theme={theme}>
+            <TextField
+              className={classes.margin}
+              InputLabelProps={{style: { color: 'grey' },}}
+              label="First Name"
+              variant="outlined"
+              id="mui-theme-provider-outlined-input"
+            />
+            
+
+            <TextField
+              className={classes.margin}
+              InputLabelProps={{style: { color: 'grey' },}}
+              label="Last Name"
+              variant="outlined"
+              id="mui-theme-provider-outlined-input"
+            />
+          </ThemeProvider>
+
+        </form>
+
+      </Container>
+
+    </div>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,91 @@
 import React, { Component } from 'react';
-import * as ReactBootStrap from "react-bootstrap";
-import main_logo from './assets/logos/01_Logo-principal.png';
+// import * as ReactBootStrap from "react-bootstrap";
+import {Navbar} from "react-bootstrap";
+import { Nav, NavDropdown} from "react-bootstrap";
+
 import main_logo2 from "./assets/logos/01_logo-horizontal.png";
 import "./Navbar.css"
 
 
-class Navbar extends Component {
+class NavBar extends Component {
 
     render() {
         return (
-        <ReactBootStrap.Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark">
-  <ReactBootStrap.Navbar.Brand href="#home"><img src={main_logo2} width="400px" class="img-fluid"/></ReactBootStrap.Navbar.Brand>
-  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-    <ReactBootStrap.Nav className="mr-auto">
-
-      {/* <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Something</ReactBootStrap.NavDropdown.Item>
-        <ReactBootStrap.NavDropdown.Divider />
-        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Separated link</ReactBootStrap.NavDropdown.Item>
-      </ReactBootStrap.NavDropdown> */}
-    </ReactBootStrap.Nav>
-    <ReactBootStrap.Nav>
-
-    <ReactBootStrap.Nav.Link href="#features" className="nav-text">HOME</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#pricing" className="nav-text">FAQ</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#deets" className="nav-text">INFO</ReactBootStrap.Nav.Link>
-      <ReactBootStrap.Nav.Link href="#memes" className="nav-text">ABOUT</ReactBootStrap.Nav.Link>
-
-    </ReactBootStrap.Nav>
-  </ReactBootStrap.Navbar.Collapse>
-</ReactBootStrap.Navbar>
+< Navbar bg="secondary" variant="dark" expand="lg">
+  < Navbar.Brand href="#home">
+      <img
+        alt=""
+        src={main_logo2}
+        width="300"
+        height="auto"
+        className="d-inline-block align-top"
+      />
+    </ Navbar.Brand>
+  < Navbar.Toggle aria-controls="basic-navbar-nav" />
+  < Navbar.Collapse id="basic-navbar-nav">
+    < Nav className="mr-auto">
+      < Nav.Link href="#home">Home</ Nav.Link>
+      < Nav.Link href="#link">Link</ Nav.Link>
+      < NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        < NavDropdown.Item href="#action/3.1">Action</ NavDropdown.Item>
+        < NavDropdown.Item href="#action/3.2">Another action</ NavDropdown.Item>
+        < NavDropdown.Item href="#action/3.3">Something</ NavDropdown.Item>
+        < NavDropdown.Divider />
+        < NavDropdown.Item href="#action/3.4">Separated link</ NavDropdown.Item>
+      </ NavDropdown>
+    </ Nav>
+    {/* < Form inline>
+      < FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      < Button variant="outline-success">Search</ Button>
+    </ Form> */}
+  </ Navbar.Collapse>
+</ Navbar>
         )
 
     }
 }
 
-export default Navbar
+export default NavBar
+
+
+// import React, { Component } from 'react';
+
+// import Form from 'react-bootstrap/Form'
+// import "./Navbar.css"
+
+
+// class Navbar extends Component {
+
+//     render() {
+//         return (
+//       <div className="auth-inner">
+//  <div className="auth-in">
+//  <Form>
+//   <Form.Group controlId="exampleForm.ControlInput1">
+//     <Form.Label>6. Name of Beneficiary</Form.Label>
+//     <Form.Control type="email" />
+//   </Form.Group>
+  
+//   <Form.Group controlId="exampleForm.ControlSelect2">
+//     <Form.Label>Example multiple select</Form.Label>
+//     <Form.Control as="select" multiple>
+//       <option>1</option>
+//       <option>2</option>
+//       <option>3</option>
+//       <option>4</option>
+//       <option>5</option>
+//     </Form.Control>
+//   </Form.Group>
+//   <Form.Group controlId="exampleForm.ControlTextarea1">
+//     <Form.Label>Example textarea</Form.Label>
+//     <Form.Control as="textarea" rows="3" />
+//   </Form.Group>
+// </Form>
+//   </div>
+//   </div>
+
+//         )
+//     }
+// }
+
+// export default Navbar
