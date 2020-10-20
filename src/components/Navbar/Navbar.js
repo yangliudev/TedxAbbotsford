d6f06ebea7f30d54769ae6e2294fd5b08f72efe8
Harish Anantharajah <37521868+harishanan@users.noreply.github.com>
2020-10-20 11:13:24 -0700

    nav color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,6 +8,37 @@ import "./Navbar.css"
 class Navbar extends Component {
 
     render() {
+
+      function active(){
+        var x = document.getElementById("home")
+        x.style.color = "red";
+        document.getElementById("faq").style.color = "white";
+        document.getElementById("info").style.color = "white";
+        document.getElementById("about").style.color = "white";
+    }
+      function active2(){
+        var x = document.getElementById("faq")
+        x.style.color = "red";
+        document.getElementById("home").style.color = "white";
+        document.getElementById("info").style.color = "white";
+        document.getElementById("about").style.color = "white";
+    }
+      function active3(){
+        var x = document.getElementById("info")
+        x.style.color = "red";
+        document.getElementById("home").style.color = "white";
+        document.getElementById("faq").style.color = "white";
+        document.getElementById("about").style.color = "white";
+    }
+      function active4(){
+        var x = document.getElementById("about")
+        x.style.color = "red";
+        document.getElementById("home").style.color = "white";
+        document.getElementById("info").style.color = "white";
+        document.getElementById("faq").style.color = "white";
+      }
+      
+
         return (
         <ReactBootStrap.Navbar collapseOnSelect expand="lg" className="color-nav" variant="dark">
   <ReactBootStrap.Navbar.Brand href={process.env.PUBLIC_URL + '/#/'}><img src={main_logo2} width="400px" class="img-fluid"/></ReactBootStrap.Navbar.Brand>
@@ -22,10 +53,10 @@ class Navbar extends Component {
       <ReactBootStrap.Nav.Link href="/faq" className="nav-text">FAQ</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link href="/info" className="nav-text">INFO</ReactBootStrap.Nav.Link>
       <ReactBootStrap.Nav.Link href="/order1" className="nav-text">ORDER</ReactBootStrap.Nav.Link> */}
-      <a href={process.env.PUBLIC_URL + '/#/'} className="nav-font">HOME</a>
-      <a href={process.env.PUBLIC_URL + '/#/faq'} className="nav-font">FAQ</a>
-      <a href={process.env.PUBLIC_URL + '/#/info'} className="nav-font">INFO</a>
-      <a href={process.env.PUBLIC_URL + '/#/about'} className="nav-font">ABOUT</a>
+      <a href={process.env.PUBLIC_URL + '/#/'} className="nav-font" id="home" onClick={active}>HOME</a>
+      <a href={process.env.PUBLIC_URL + '/#/faq'} className="nav-font" id="faq" onClick={active2}>FAQ</a>
+      <a href={process.env.PUBLIC_URL + '/#/info'} className="nav-font" id="info" onClick={active3}>INFO</a>
+      <a href={process.env.PUBLIC_URL + '/#/about'} className="nav-font" id="about" onClick={active4}>ABOUT</a>
 
     </ReactBootStrap.Nav>
   </ReactBootStrap.Navbar.Collapse>
