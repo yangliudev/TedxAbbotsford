c01557057d739e136971beb6ef04b4ff2e7c2604
AnthonyQR <55644542+AnthonyQR@users.noreply.github.com>
2020-10-20 17:27:10 -0700

    Added mobile responsiveness for the homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,31 +8,23 @@ class Home extends Component {
     render() {
         return (
             <ReactBootStrap.Container className="top-space">
-                <ReactBootStrap.Row>
-                    <ReactBootStrap.Col>
-                        <div>
-                            <img src={tower} className="tower"></img>
-                        </div>
-                    </ReactBootStrap.Col>
+                <div class="firstsection">
+                    <div class="towerDiv">
+                        <img src={tower} className="tower"></img>
+                    </div>
 
-                    <ReactBootStrap.Col>
-                        <div class ="textBG">
-                            <ReactBootStrap.Row>
-                                <div class="textAlign">
-                                    <h1 class="towerText"><span class="firstTowerText">OFFER A MUSICIAN AT HOME <br/>TO THE PEOPLE YOU LOVE,<br/></span> <span class="secondTowerText">IN LESS THAN 3 MINUTES!</span></h1>
-                                </div>
-                            </ReactBootStrap.Row>
-                            <ReactBootStrap.Row>
-                                <div class="buttonAlign">
-                                <a href={process.env.PUBLIC_URL + '/#/order'}>
-                                    <input type="button" value="Order Now" class="orderButton"></input>
-                                </a>
-                                </div>
-                            </ReactBootStrap.Row>
-                        </div>
-                    </ReactBootStrap.Col>
-            
-                </ReactBootStrap.Row>
+                    <div class ="textBG">
+                            <div class="textAlign">
+                                <h1 class="towerText"><span class="firstTowerText">OFFER A MUSICIAN AT HOME <br/>TO THE PEOPLE YOU LOVE,<br/></span> <span class="secondTowerText">IN LESS THAN 3 MINUTES!</span></h1>
+                            </div>
+                        
+                            <div class="buttonAlign">
+                            <a href={process.env.PUBLIC_URL + '/#/order'}>
+                                <input type="button" value="Order Now" class="orderButton"></input>
+                            </a>
+                            </div>
+                    </div>
+                </div>
             </ReactBootStrap.Container>
         )
     };
