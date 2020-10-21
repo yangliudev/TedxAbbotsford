a9f86c623bc346cba4ed799fad38fe455c54ee56
Harish Anantharajah <37521868+harishanan@users.noreply.github.com>
2020-10-20 20:40:34 -0700

    basic working database

--- server.js
+++ server.js
@@ -1,14 +1,102 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
-app.get('/musician', (req, res) => {
-    const musicians = [
-        {id: 1, firstName: 'John', lastName: 'Doe'},
-        {id: 2, firstName: 'Mary', lastName: 'Goe'},
-        {id: 3, firstName: 'Dame', lastName: 'Dalla'},
-    ];
-    res.json(musicians);
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Anantharajah123!',
+    database: 'musician'
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// app.get('/', (req, res) => {
+//     // const sqlInsert = "INSERT INTO musician_table (name, type) VALUES ('Trump', 'bad')"
+//     // const musicians = [
+//     //     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     //     {id: 2, firstName: 'Mary', lastName: 'Goe'},
+//     //     {id: 3, firstName: 'Dame', lastName: 'Dalla'},
+//     // ];
+//     // db.query(sqlInsert, (err, result) => {
+//     //     res.json(musicians);
+//     // })
+// });
+
+
+app.get("/api/get", (req, res) => {
+    const sqlSelect = "SELECT * FROM musician_table";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    }); 
+});
+
+app.post("/api/insert", (req, res)=> {
+    const musicianName = req.body.musicianName;
+    const musicianType = req.body.musicianType;
+
+    const sqlInsert = "INSERT INTO musician_table (name, type) VALUES (?, ?)"
+    db.query(sqlInsert, [musicianName, musicianType], (err, result)=> {
+        console.log(result);
+    }); 
+});
+
+
+app.post("/order/insert", (req, res)=> {
+    const orderGift = req.body.orderGift;
+    const orderOccasion = req.body.orderOccasion;
+    const orderType = req.body.orderType;
+    const orderNumberMusicians = req.body.orderNumberMusicians;
+    const orderSuprise = req.body.orderSuprise;
+    const orderFirstName = req.body.orderFirstName;
+    const orderLastName = req.body.orderLastName;
+    const orderDateService = req.body.orderDateService;
+    const orderTimeService = req.body.orderTimeService;
+    const orderOffered = req.body.orderOffered;
+    const orderNumber = req.body.orderNumber;
+    const orderEmail = req.body.orderEmail;
+    const orderAddress = req.body.orderAddress;
+    const orderAddress2 = req.body.orderAddress2;
+    const orderCity = req.body.orderCity;
+    const orderState = req.body.orderState;
+    const orderZip = req.body.orderZip;
+    const orderComments = req.body.orderComments;
+
+    const sqlInsert = "INSERT INTO ordering_table (gift, occasion, type, number_musicians, suprise, firstName, lastName, date_service, time_service, offered, number, email, address, address_2, city, state, zip, comments) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    db.query(sqlInsert, [
+        orderGift, 
+        orderOccasion, 
+        orderType, 
+        orderNumberMusicians, 
+        orderSuprise, 
+        orderFirstName, 
+        orderLastName, 
+        orderDateService, 
+        orderTimeService, 
+        orderOffered,
+        orderNumber,
+        orderEmail,
+        orderAddress,
+        orderAddress2,
+        orderCity,
+        orderState,
+        orderZip,
+        orderComments
+    ],
+         (err, result)=> {
+        console.log(result);
+        console.log(err);
+    }); 
+});
+
+app.get("/order/get", (req, res) => {
+    const sqlSelect = "SELECT * FROM ordering_table";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    }); 
 });
 
 const port = 5000;
--- src/components/Ordering/Ordering_1.js
+++ src/components/Ordering/Ordering_1.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import * as ReactBootStrap from "react-bootstrap";
+import Axios from 'axios';
+
 import "./Ordering.css"
 import pic1 from "./ordering-assets/GRANDMERE2.png"
 import pic2 from "./ordering-assets/02-picto-artiste.png"
@@ -18,8 +20,8 @@ import pic10 from "./ordering-assets/duomusiciens.png"
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col'
 
-class Ordering_1 extends Component {
-    render() {
+
+function Ordering_1() {
         function hide1(){
             var x = document.getElementById("order1")
             x.style.display = "none";
@@ -45,6 +47,76 @@ class Ordering_1 extends Component {
             x.style.display = "none";
             document.getElementById("order6").style.display = "block"
         }
+
+
+
+        const [musicianName, setMusicianName] = useState("");
+        const [musicianType, setMusicianType] = useState("");
+        const [musicanList, setMusicanList] = useState([]);
+
+
+        const [gift, setGift] = useState("");
+        const [occasion, setOccasion] = useState("");
+        const [type, setType] = useState("");
+        const [numberMusicians, setNumberMusicians] = useState("");
+        const [suprise, setSuprise] = useState("");
+        const [firstName, setFirstName] = useState("");
+        const [lastName, setLastName] = useState("");
+        const [dateService, setDateService] = useState("");
+        const [timeService, setTimeService] = useState("");
+        const [offered, setOffered] = useState("");
+        const [number, setNumber] = useState("");
+        const [email, setEmail] = useState("");
+        const [address, setAddress] = useState("");
+        const [address2, setAddress2] = useState("");
+        const [city, setCity] = useState("");
+        const [state, setState] = useState("");
+        const [zip, setZip] = useState("");
+        const [comments, setComments] = useState("");
+
+        const submitOrder = () => {
+          Axios.post("http://localhost:5000/order/insert", {
+            orderGift: gift,
+            orderOccasion: occasion,
+            orderType: type,
+            orderNumberMusicians: numberMusicians,
+            orderSuprise: suprise,
+            orderFirstName: firstName,
+            orderLastName: lastName,
+            orderDateService: dateService,
+            orderTimeService: timeService,
+            orderOffered: offered,
+            orderNumber: number,
+            orderEmail: email,
+            orderAddress: address,
+            orderAddress2: address2,
+            orderCity: city,
+            orderState: state,
+            orderZip: zip,
+            orderComments: comments
+        }).then(()=> {
+          alert("sucessful insert");
+        })
+        };
+        
+
+
+        useEffect(() => {
+          Axios.get("http://localhost:5000/api/get").then((response) => {
+            // console.log(response.data)
+            setMusicanList(response.data)
+          })
+        })
+
+        const submitMusician = () => {
+          Axios.post("http://localhost:5000/api/insert", {
+            musicianName: musicianName, 
+            musicianType: musicianType
+        }).then(()=> {
+          alert("sucessful insert");
+        })
+        };
+
         return (
             
             <ReactBootStrap.Container className="top-space">
@@ -53,17 +125,30 @@ class Ordering_1 extends Component {
                 <ReactBootStrap.Col><h3>1. Who do you wish to give this gift to?</h3></ReactBootStrap.Col>
             </ReactBootStrap.Row>
             <ReactBootStrap.Row>
+              {/* <input type="text" name="musicianName" onChange={(e) => {setMusicianName(e.target.value);}}></input>
+              {/* <input type="text" name="musicianType" onChange={(e) => {setMusicianType(e.target.value);}}></input> */}
+              {/* <Form.Control placeholder="yourname@gmail.com" onChange={(e) => {setMusicianType(e.target.value);}}/> */}
+              {/* <button onClick={submitMusician}>Submit</button> */}
+
+
+              {/* {musicanList.map((val) => {
+                return <h5>{val.name}<br /></h5>
+              })} */}
+
+
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide1}><img src={pic1} className="pic"/><p>One Person In Particular</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col>
+                      <a onClick={(e) => {setGift("one person"); hide1();}}><img src={pic1} className="pic"/><p>One Person In Particular</p></a>
+                      </ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide1}><img src={pic2} className="pic"/><p>A Small Committee</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setGift("small committee"); hide1();}}><img src={pic2} className="pic"/><p>A Small Committee</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide1}><img src={pic3} className="pic"/><p>An <br/>Institution</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setGift("institution"); hide1();}}><img src={pic3} className="pic"/><p>An <br/>Institution</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide1}><img src={pic4} className="pic"/><p>Donate To The Platform</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setGift("donate"); hide1();}}><img src={pic4} className="pic"/><p>Donate To The Platform</p></a></ReactBootStrap.Col>
                     </div>
             </ReactBootStrap.Row>
                 </div>
@@ -81,7 +166,7 @@ class Ordering_1 extends Component {
                 <ReactBootStrap.Row>
                     <ReactBootStrap.Col style={{marginLeft:'20px', marginRight:'20px'}}>                    
                     <div class="md-form">
-                        <input type="text" id="form1" class="form-control" />
+                        <input type="text" id="form1" class="form-control" onChange={(e) => {setOccasion(e.target.value);}}/>
                         </div>
                         </ReactBootStrap.Col>
                 </ReactBootStrap.Row>
@@ -104,16 +189,16 @@ class Ordering_1 extends Component {
             </ReactBootStrap.Row>
             <ReactBootStrap.Row>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide3}><img src={pic5} className="pic"/><p>Classic</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setType("classic"); hide3();}}><img src={pic5} className="pic"/><p>Classic</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide3}><img src={pic6} className="pic"/><p style={{fontSize:'19px'}}>Popular Music</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setType("popular"); hide3();}}><img src={pic6} className="pic"/><p style={{fontSize:'19px'}}>Popular Music</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide3}><img src={pic7} className="pic"/><p>Jazzy - Pop</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setType("jazzy/pop"); hide3();}}><img src={pic7} className="pic"/><p>Jazzy - Pop</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide3}><img src={pic8} className="pic"/><p>Suprise!</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setType("suprise"); hide3();}}><img src={pic8} className="pic"/><p>Suprise!</p></a></ReactBootStrap.Col>
                     </div>
             </ReactBootStrap.Row>
                 </div>
@@ -131,10 +216,10 @@ class Ordering_1 extends Component {
             </ReactBootStrap.Row>
             <ReactBootStrap.Row sm={1} md={3}>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide4}><img src={pic9} className="pic2"/><p>Soloist</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setNumberMusicians("solo"); hide4();}}><img src={pic9} className="pic2"/><p>Soloist</p></a></ReactBootStrap.Col>
                     </div>
                     <div className="bg-display-test">
-                    <ReactBootStrap.Col><a onClick={hide4}><img src={pic10} className="pic2"/><p>Duo</p></a></ReactBootStrap.Col>
+                    <ReactBootStrap.Col><a onClick={(e) => {setNumberMusicians("duo"); hide4();}}><img src={pic10} className="pic2"/><p>Duo</p></a></ReactBootStrap.Col>
                     </div>
             </ReactBootStrap.Row>
                 </div>
@@ -148,13 +233,13 @@ class Ordering_1 extends Component {
                 <ReactBootStrap.Col><h3>5. Is it a suprise?</h3></ReactBootStrap.Col>
             </ReactBootStrap.Row>
             <ReactBootStrap.Row sm={1} md={6}>
-            <a onClick={hide5}><div className="bg-display-button">
+            <a onClick={(e) => {setSuprise("yes"); hide5();}}><div className="bg-display-button">
                     <ReactBootStrap.Col><p>Yes</p></ReactBootStrap.Col>
                     </div>
                     </a>
             </ReactBootStrap.Row>
             <ReactBootStrap.Row sm={1} md={6}>
-            <a onClick={hide5}>
+            <a onClick={(e) => {setSuprise("no"); hide5();}}>
                     <div style={{marginTop:'8px', paddingRight:"5px"}} className="bg-display-button">
                     <ReactBootStrap.Col><p>No</p></ReactBootStrap.Col>
                     </div>
@@ -174,12 +259,12 @@ class Ordering_1 extends Component {
   <Form.Row className="justify-content-md-center">
     <Form.Group as={Col} xs={7} controlId="formGridFirstName">
       <Form.Label className="a">Beneficiary First Name</Form.Label>
-      <Form.Control type="text" placeholder="First Name" />
+      <Form.Control type="text" placeholder="First Name" onChange={(e) => {setFirstName(e.target.value);}}/>
     </Form.Group>
   
     <Form.Group as={Col}  controlId="formGridLastName">
     <Form.Label>Last Name</Form.Label>
-      <Form.Control type="text" placeholder="Last Name" />
+      <Form.Control type="text" placeholder="Last Name" onChange={(e) => {setLastName(e.target.value);}}/>
     </Form.Group>
   </Form.Row>
 
@@ -187,51 +272,51 @@ class Ordering_1 extends Component {
   <Form.Group controlId="formGridDate">
     <Form.Label>Date of Service</Form.Label>
     <h6><i>The order must be placed at least 72 hours in advance to be considered.</i></h6>
-    <Form.Control type="date" placeholder="1234 Main St" />
+    <Form.Control type="date" placeholder="1234 Main St" onChange={(e) => {setDateService("2020-01-01 10:10:10");}}/>
   </Form.Group>
   
   <Form.Group controlId="formGridTime">
     <Form.Label>Time of Service</Form.Label>
     <h6><i>Enter the time between 08:00 and 21:00 maximum</i></h6>
-    <Form.Control type="time" placeholder="1234 Main St" />
+    <Form.Control type="time" placeholder="1234 Main St" onChange={(e) => {setTimeService(e.target.value);}}/>
   </Form.Group>
 
   <Form.Group controlId="formGridHost">
     <Form.Label>Offered by</Form.Label>
-    <Form.Control type="text" placeholder="Answer here" />
+    <Form.Control type="text" placeholder="Answer here" onChange={(e) => {setOffered(e.target.value);}}/>
   </Form.Group>
 
   <Form.Group controlId="formGridPhone">
     <Form.Label>Contact Number</Form.Label>
     <h6><i>Please enter a canadian number</i> </h6>
-    <Form.Control type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="012-345-6789" />
+    <Form.Control type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="012-345-6789" onChange={(e) => {setNumber(e.target.value);}}/>
   </Form.Group>
 
   <Form.Group controlId="formGridEmail">
     <Form.Label>Email Address</Form.Label>
-    <Form.Control placeholder="yourname@gmail.com" />
+    <Form.Control placeholder="yourname@gmail.com" onChange={(e) => {setEmail(e.target.value);}}/>
   </Form.Group>
 
 
   <Form.Group controlId="formGridAddress1">
     <Form.Label>Address 1</Form.Label>
-    <Form.Control placeholder="1234 Main St" />
+    <Form.Control placeholder="1234 Main St" onChange={(e) => {setAddress(e.target.value);}}/>
   </Form.Group>
 
   <Form.Group controlId="formGridAddress2">
     <Form.Label>Address 2</Form.Label>
-    <Form.Control placeholder="Apartment, studio, or floor" />
+    <Form.Control placeholder="Apartment, studio, or floor" onChange={(e) => {setAddress2(e.target.value);}}/>
   </Form.Group>
   </div>
   <Form.Row>
     <Form.Group as={Col} controlId="formGridCity">
       <Form.Label>City</Form.Label>
-      <Form.Control />
+      <Form.Control onChange={(e) => {setCity(e.target.value);}}/>
     </Form.Group>
 
     <Form.Group as={Col} controlId="formGridState">
       <Form.Label>State</Form.Label>
-      <Form.Control as="select" defaultValue="Choose...">
+      <Form.Control as="select" defaultValue="Choose..." onChange={(e) => {setState(e.target.value);}}>
         <option>British Columbia</option>
         <option>Alberta</option>
       </Form.Control>
@@ -239,17 +324,17 @@ class Ordering_1 extends Component {
 
     <Form.Group as={Col} controlId="formGridZip">
       <Form.Label>Zip</Form.Label>
-      <Form.Control />
+      <Form.Control onChange={(e) => {setZip(e.target.value);}}/>
     </Form.Group>
   </Form.Row>
 
   <Form.Group controlId="formGridComment">
     <Form.Label>Additional Comments</Form.Label>
     <h6><i>Leave us a little comment that will help us better target the event and the artist.</i></h6>
-    <Form.Control as="textarea" rows={3} placeholder="Anything you'd like us to know" />
+    <Form.Control as="textarea" rows={3} placeholder="Anything you'd like us to know" onChange={(e) => {setComments(e.target.value);}}/>
   </Form.Group>
   <a href={process.env.PUBLIC_URL + '/#/'}>
-  <ReactBootStrap.Button variant="danger" className='button'>Submit</ReactBootStrap.Button></a>
+  <ReactBootStrap.Button variant="danger" className='button' onClick={submitOrder}>Submit</ReactBootStrap.Button></a>
 </Form>  
 </div>
 
@@ -287,6 +372,5 @@ class Ordering_1 extends Component {
         
         )
     };
-};
 
 export default Ordering_1
